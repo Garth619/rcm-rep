fce9a49c3cbf62689bfeb85fdd2a919d19ea59f6
Garrett Cullen <Garth619@users.noreply.github.com>
2019-04-06 13:43:40 -0700

    push

--- wp-content/themes/rcmrep/page-templates/template-fullpage.php
+++ wp-content/themes/rcmrep/page-templates/template-fullpage.php
@@ -5,6 +5,16 @@
 	get_header(); ?>
 
 
+<section id="top_banner">
+	
+	<div class="top_banner_inner"></div><!-- top_banner_inner -->
+	
+</section><!-- top_banner -->
 		
+
+
+
+
+
 
 <?php get_footer(); ?>
