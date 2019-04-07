50ae00c00b2913ed6ca0d733a8c230ffa1c517b1
Garrett Cullen <Garth619@users.noreply.github.com>
2019-04-06 19:04:36 -0700

    push

---
+++ wp-content/themes/rcmrep/template-fullpage/sections-fullpage/section-formbox.php
@@ -0,0 +1,5 @@
+<section id="formbox" class="rep_box">
+	
+	<h2 class="fullpage_h2">Lorem ipsum dolor sit amet, cons ect etur adip icing elit, sed do eius</h2><!-- fullpage_h2 -->
+	
+</section>
--- wp-content/themes/rcmrep/template-fullpage/template-fullpage.php
+++ wp-content/themes/rcmrep/template-fullpage/template-fullpage.php
@@ -4,9 +4,8 @@
 	
 	get_header();
 	
-	
 	get_template_part('template-fullpage/sections-fullpage/section','topbanner');
 	get_template_part('template-fullpage/sections-fullpage/section','socialmediabox');
-
+	get_template_part('template-fullpage/sections-fullpage/section','formbox');
 	
 	get_footer(); ?>
