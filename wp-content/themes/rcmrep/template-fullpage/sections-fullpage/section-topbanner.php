e0baef77eb2f50e2ac6a25509f29ac23f25d83a7
Garrett Cullen <Garth619@users.noreply.github.com>
2019-04-09 19:44:05 -0700

    push

--- wp-content/themes/rcmrep/template-fullpage/sections-fullpage/section-topbanner.php
+++ wp-content/themes/rcmrep/template-fullpage/sections-fullpage/section-topbanner.php
@@ -1,41 +1,23 @@
 
 <section id="top_banner">
 	
+	<img class="top_banner_img" src="<?php bloginfo('template_directory');?>/images/top_banner_img.jpg"/>
+	
 	<div class="top_banner_inner">
 		
 		<h1>Lorem ipsum dolor sit amet, consect etur adip icing elit, sed do eius?</h1>
 		
 		<div class="five_star_wrapper">
 			
-			<span class="star">
+			<span class="star">★</span><!-- star -->
 			
-				<?php echo file_get_contents("wp-content/themes/rcmrep/images/star.svg"); ?>
+			<span class="star">★</span><!-- star -->
 			
-			</span><!-- star -->
+			<span class="star">★</span><!-- star -->
 			
-			<span class="star">
+			<span class="star">★</span><!-- star -->
 			
-				<?php echo file_get_contents("wp-content/themes/rcmrep/images/star.svg"); ?>
-			
-			</span><!-- star -->
-			
-			<span class="star">
-			
-				<?php echo file_get_contents("wp-content/themes/rcmrep/images/star.svg"); ?>
-			
-			</span><!-- star -->
-			
-			<span class="star">
-			
-				<?php echo file_get_contents("wp-content/themes/rcmrep/images/star.svg"); ?>
-			
-			</span><!-- star -->
-			
-			<span class="star">
-			
-				<?php echo file_get_contents("wp-content/themes/rcmrep/images/star.svg"); ?>
-			
-			</span><!-- star -->
+			<span class="star">★</span><!-- star -->
 			
 		</div><!-- five_star_wrapper -->
 		
