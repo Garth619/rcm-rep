fb07395e54f22ddc8af9ff8393eb5dfebc41fa76
Garrett Cullen <Garth619@users.noreply.github.com>
2019-04-14 20:29:28 -0700

    push

--- wp-content/themes/rcmrep/404.php
+++ wp-content/themes/rcmrep/404.php
@@ -1,16 +1,15 @@
 <?php get_header(); ?>
 
-<h1>Not Found</h1>
-		
-		<p>The page you were looking for appears to have been moved, deleted or does not exist. You could <span class="go_back" onclick="goBack()">go back</span> to where you were or head straight to our <a href="/">home page</a>.</p>
-		
-		
-	<script type="text/javascript">
-		function goBack() {
-    	window.history.back();
-		}
-	</script>
+	<section id="not_found" class="section_padding">
 
+		<div class="rep_box">
+
+		<h1>Not Found</h1>
+		
+		<p>The page you were looking for does not exist</p>
+		
+		</div>
 	
-
+	</section><!-- not_found -->
+	
 <?php get_footer(); ?>
