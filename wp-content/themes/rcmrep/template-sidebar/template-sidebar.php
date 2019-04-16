d93359ddb109a12ec7274e62d23422023f9c1555
Garrett Cullen <Garth619@users.noreply.github.com>
2019-04-15 21:31:43 -0700

    push

--- wp-content/themes/rcmrep/template-sidebar/template-sidebar.php
+++ wp-content/themes/rcmrep/template-sidebar/template-sidebar.php
@@ -4,11 +4,28 @@
 	
 	get_header();?>
 	
+	<div class="sidebar_test">
+		
+		<section id="sidebar">
+			
+			<div class="rep_box">
+				
+				<h2 class="sidebar_h2">Lorem ipsum dolor sit amet, consect etur?</h2><!-- sidebar_h2 -->
+				
+				<a class="sidebar_review_button" href="" target="_blank" rel="noopener">
+					
+					<span>Leave A Review</span>
+				
+				</a><!-- sidebar_review_button -->
+				
+			</div><!-- rep_box -->
+			
+		</section><!-- sidebar -->
+		
+	</div><!-- sidebar_test -->
 	
 	
-	
-	
-<?php wp_footer();?>
+	<?php wp_footer();?>
 
 
 </body>
